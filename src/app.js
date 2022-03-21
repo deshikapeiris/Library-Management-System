4b88be764b0ec03731bc99d6b91e59ce3e678cb6
deshika peiris <deshikaprabhashi422@gmail.com>
2022-03-21 12:52:18 +0530

    Initilize project

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 
 app.get("/", (req, res, next) => {
-    res.send("<h2>Library system api</h2>");
+    res.send("<h2> 📚 Library system api</h2>");
     next();
 });
 
 app.listen(PORT, () => {
-    logger.info(`Sever is up and running on PORT ${PORT}`);
+    logger.info(`🌻 Sever is up and running on PORT ${PORT}`);
     connect();
 });
