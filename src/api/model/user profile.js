69de026046d1594c888e91352ed91b8d324dd577
deshika peiris <deshikaprabhashi422@gmail.com>
2022-03-30 17:30:54 +0530

    implementing profile

---
+++ src/api/model/user profile.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    
+    regNo:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    dob :{
+        type:String,
+        required:true
+    },
+
+     address :{
+        type:String,
+        required:true
+    },
+
+
+	email :{
+        type:String,
+        required:true
+    },
+	
+	 phoneNo :{
+        type:String,
+        required:true
+    },
+	
+    nic:{
+        type:String,
+        required:true
+    },
+
+    shift :{
+        type:String,
+        required:true
+    },
+   
+    regDate :{
+        type:String,
+        required:true
+   
+    }
+
+})
+
+const user = mongoose.model("user profile",userSchema);
+module.exports = user;
